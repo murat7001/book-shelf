940642a82b1dcda50dd273df2b130e1a6ce01e84
Cagritrkmen <cagritrkmn33@gmail.com>
2023-11-15 12:02:17 +0300

    add search feature

---
+++ src/components/Search.js
@@ -0,0 +1,79 @@
+
+import React, { useEffect, useState } from 'react';
+
+import {
+    Stack,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    Button,
+} from '@mui/material';
+import { fetchBookWithSearch, fetchBooks } from '@/pages/api/api';
+const Search= ({ setBooks })=> {
+
+    const [searchText, setSearchText] = useState('');
+    const [searchBy, setSearchBy] = useState('title');
+
+    const fetchBookData = () => {
+        const fetchFunction = searchText
+            ? fetchBookWithSearch
+            : fetchBooks;
+
+        fetchFunction(searchText, searchBy)
+            
+            .then((bookList) => {
+                console.log(searchText,searchBy)
+                setBooks(bookList)
+                console.log(bookList)
+
+            })
+            .catch((error) => {
+                console.log(error.message || 'Bir hata oluştu');
+            })
+
+    };
+    useEffect(() => {
+        fetchBookData();
+    }, []);
+    return (
+        <Stack alignItems={"flex-start"} direction="row" spacing={2} >
+            <FormControl color='info' size="small" sx={{ width: '150px' }} variant='standard'>
+                <InputLabel id="demo-simple-select-label">SearchBy</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={searchBy}
+                    label="searchBy"
+                    onChange={(e) => setSearchBy(e.target.value)}
+
+                >
+                    <MenuItem value="title">Title</MenuItem>
+                    <MenuItem value="author">Author</MenuItem>
+                </Select>
+
+            </FormControl>
+            <TextField sx={{ color: "white" }}
+                id="outlined-basic"
+                label="Search"
+                variant="filled"
+                size="small"
+                fullWidth
+                placeholder="Enter search text"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+            />
+            <Button sx={{ backgroundColor: "green", color: "white", padding: "10px" }}
+                variant="contained"
+                color="primary"
+                onClick={fetchBookData}
+
+            >
+                Search
+            </Button>
+        </Stack>
+    )
+}
+
+export default Search
--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -4,10 +4,20 @@ import axios from "axios";
 
 export const deleteBook = async (id) => {
 
-    try {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-      } catch (error) {
-        alert('Failed to delete book');
-        console.error('An error occurred:', error);
-      }
-  };
+  try {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+  } catch (error) {
+    alert('Failed to delete book');
+    console.error('An error occurred:', error);
+  }
+};
+
+export const fetchBooks = async () => {
+  const response = await axios.get( "http://localhost:3001/books");
+  return response.data;
+};
+
+
+export const fetchBookWithSearch = async (searchText, searchBy) => {
+  const response = await axios.get(`http://localhost:3001/books/?${searchBy}=${searchText}`)
+  return response.data;}
